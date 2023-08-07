4de806cf593de55f810528afa9ebe5be14425b7a
axlowski <axlowski@gmail.com>
2023-08-08 00:06:20 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const age = 43;
 console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('.page-header__heading--js');
+header.innerHTML = 'Hej!';
+
+console.log(header.style)
+
+header.style.color = 'red';
