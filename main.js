9e76deff18a6ffb3cd33861076b134d061bea820
axlowski <axlowski@gmail.com>
2023-08-08 00:21:52 +0200

    change header

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const firstName = 'Daro';
-const age = 43;
+const firstName = 'Dar';
+const age = 'xx';
 console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector('.page-header__heading--js');
-header.innerHTML = 'Hej!';
-
-console.log(header.style)
-
-header.style.color = 'red';
