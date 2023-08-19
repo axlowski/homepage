d19b1e6f1a78d188d7dfcd938c594c12af409dc5
axlowski <axlowski@gmail.com>
2023-08-19 14:13:40 +0200

    Content & main.js

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector('.page-header__heading--js');
+
+header.innerHTML = 'Hello Wolrd!!';
+
+console.log(header.style);
+
+header.style.color = 'red';
+
