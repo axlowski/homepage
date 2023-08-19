b43df6f3e708b2b5e58bd83badd778d283ed823d
axlowski <axlowski@gmail.com>
2023-08-19 15:07:09 +0200

    M04-Homework

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector('.page-header__heading--js');
 
-header.innerHTML = 'Hello Wolrd!!';
+header.innerHTML = 'Hello World!!';
 
 console.log(header.style);
 
 header.style.color = 'red';
+
+const paragraph = document.querySelector('.paragraph--js');
+
+paragraph.innerHTML = 'Treść paragrafu wstawiona z JS';
 
